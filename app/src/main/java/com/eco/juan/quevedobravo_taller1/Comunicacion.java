c2267d85425252fd3bcc6c693a0df295a7e1b6ee
Juan Bravo <juanda.bravo98@gmail.com>
2019-03-22 22:56:27 -0500

    Análogo Android
    Análogo básico funcional en Android Studio

---
+++ app/src/main/java/com/eco/juan/quevedobravo_taller1/Comunicacion.java
@@ -0,0 +1,75 @@
+package com.eco.juan.quevedobravo_taller1;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Observable;
+
+public class Comunicacion extends Observable implements Runnable {
+
+    private Socket socket;
+    private DataInputStream entrada;
+    private DataOutputStream salida;
+    private boolean conectado;
+
+    private static Comunicacion ref;
+
+    private Comunicacion() {
+        conectado = false;
+        System.out.print("Esperando Conexión");
+    }
+
+    public static Comunicacion getRef() {
+        if(ref == null) {
+            ref = new Comunicacion();
+            Thread t = new Thread(ref);
+            t.start();
+        }
+        return ref;
+    }
+
+    public void run() {
+        while(true) {
+            try {
+                if(!conectado) {
+
+                    socket = new Socket(InetAddress.getByName("192.168.1.46"), 5000);
+                    entrada = new DataInputStream(socket.getInputStream());
+                    salida = new DataOutputStream(socket.getOutputStream());
+                    conectado = true;
+                } else {
+                    recibir();
+                    Thread.sleep(33);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void recibir() throws IOException {
+        String mensaje = entrada.readUTF();
+        setChanged();
+        notifyObservers(mensaje);
+        clearChanged();
+    }
+
+    public void enviar(final String id) {
+        if(socket != null && socket.isConnected() && salida != null) {
+            new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        salida.writeUTF(id);
+                        salida.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+    }
+}
---
+++ app/src/main/java/com/eco/juan/quevedobravo_taller1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.eco.juan.quevedobravo_taller1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import io.github.controlwear.virtual.joystick.android.JoystickView;
+
+public class MainActivity extends AppCompatActivity implements Observer {
+
+    private JoystickView analogo;
+    private Comunicacion ref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ref = Comunicacion.getRef();
+        ref.addObserver(this);
+
+        analogo = findViewById(R.id.jv_analogo_main);
+
+        analogo.setOnMoveListener(new JoystickView.OnMoveListener() {
+            public void onMove(int angle, int strength) {
+                if(strength > 20) {
+                    ref.enviar("Mover: :"+angle);
+                }
+            }
+        });
+    }
+
+    public void update(Observable o, Object arg) {
+
+    }
+}
